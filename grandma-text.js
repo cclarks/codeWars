4af0996067bc3f92f30138f0686657b1a27084c9
Cody <c.susie.run@gmail.com>
2017-11-09 11:47:35 -0500

    grandma text javascript finished

---
+++ grandma-text.js
@@ -0,0 +1,14 @@
+// write a function that replaces 'two', 'too' and 'to' with the number '2'. Even if the sound is found mid word
+// (like in octopus) grandma still thinks that should be replaced with a 2. bless her.
+//
+// 'I love to text' becomes 'I love 2 text' 'see you tomorrow' becomes 'see you 2morrow' 'look at that octopus' becomes 'look at that oc2pus'
+//
+// Note that 'too' should become '2', not '2o'.
+//
+
+
+function textin(str){
+  console.log(str);
+  var patt = /(too|two|to)/gi;
+  return str.replace(patt, "2");
+}
