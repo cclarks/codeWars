c6b65c851a5d956ef3d32c52b065e969fa175d56
Cody <c.susie.run@gmail.com>
2017-12-04 15:49:15 -0500

    Even Or Odd. + New One That Working on

---
+++ apple-substitution.js
@@ -0,0 +1,21 @@
+// In order to sell more of its produce, a local farm sells bags of apples in two sizes: medium and large.
+// A medium bag contains 4 Macintosh and 1 Granny Smith apples and costs $2.80. A large bag contains 8 Macintosh
+// and 4 Granny Smith apples and costs $7.20. The price of one Granny Smith apple is the same in the medium bag as
+// it is in the large bag. The price of one Macintosh apple is the same in the medium bag as it is in the large bag.
+// What is the price of each kind of apple?
+
+medium = 4 macintosh 1 granny smith = $2.80
+
+large = 8 macintosh 4 granny smith = $7.20
+
+price of one granny smith is the same in medium as large
+
+price of one macintosh is same in medium as large
+
+what is price?
+
+Cost of Macintosh apples + Cost of Granny Smith apples = Total cost of bag
+
+4m + g = 2.80
+
+8m + 4g = 7.20
